4ea1bce8dd6d8ae3af9c7cae9d8a9f5f50452eae
Enzo Rodrigues <enzorodrigues272@gmail.com>
2022-06-22 20:47:51 -0300

    Refatorando código

--- main.py
+++ main.py
@@ -1,95 +1,125 @@
 import os
 
-logMemoria = []  # bufferLog
-logDisco = []  # Log do disco
-bufferDados = [7, 14, 21, 28, 35]  # Buffer de dados da memoria
-discoDados = [7, 14, 21, 28, 35]  # Dados do disco
-redo = []
-undo = []
-aux = None
+logDisco = []  # log do disco
+logMemoria = [] # buffer do log
+memoriaDado = [5000,7000,9000,11000,13000] # buffer de dados 
+discoDado = [5000,7000,9000,11000,13000] # dados do disco
+redo = [] # lista para armazenar transações redo
+undo = [] # lista para armazenar transações undo
+previaMemoria = [] # lista para armazenar os valores antigos e fazer undu no disco
+arquivoDiscoDados = 'discodado.txt' # arquivo de texto para receber valores que ficam no dado dos disco
+arquivoDiscoLog = 'discolog.txt' # arquivo de texto para receber valores que ficam no log do disco
 
-
-def viewLogMemoria():
-    print("Visualizando Log da memória\n")
-    print(logMemoria, "\n")
-
-
-def viewLogDisco():
-    print("Visualizando Log do disco\n")
-    print(logDisco, "\n")
-
-
-def viewDadosMemoria():
-    print("Visualizando Dados da memoria\n")
-    print(bufferDados, "\n")
-
-
-def viewDadosDisco():
-    print("Visualizando Dados da memoria\n")
-    print(discoDados, "\n")
-
-
-def update():
-    t = input("Escolha a transação: ")
-    i = int(input("Digite a posicao do elemento: "))
-    newValue = int(input("Digite a idade atualizada: "))
-    logMemoria[:] += ([t, i, "idade", discoDados[i - 1], newValue])
-    bufferDados[i - 1] = newValue
-
-
-def checkpoint():
-    logDisco[:] = logMemoria[:]
-    discoDados[:] = bufferDados[:]
-
-
-def failure():
-    logMemoria.clear()
-    logDisco.clear()
-    bufferDados.clear()
 
 
 def commit():
-    t = input("Digite a transação que deseja commitar: ")
-    logDisco[:] += ([s for s in logMemoria if t in s])
-    redo[:] += ([s for s in logMemoria if t in s])  #
-    if os.path.exists('LogDisco.txt'):
-        f = open("LogDisco.txt", "w")
-        # for i in discoLog:
-        f.write(str(logDisco) + "\n")
-        f.close()
+        t = input("Digite a transação que deseja commitar: ")
+        logDisco[:] += ([s for s in logMemoria if t in s])
+        redo[:] += ([s for s in logMemoria if t in s]) #
+        if(os.path.exists(arquivoDiscoLog)):
+            f = open(arquivoDiscoLog, "w")
+            for i in logDisco:
+                f.write(str(i)+"\n")
+            f.close()
 
+def checkpoint():
+        if(os.path.exists(arquivoDiscoDados)):
+            f = open(arquivoDiscoDados, "w")
+            f.write(str(memoriaDado[:])+"\n")
+            f.close
+            previaMemoria[:] += memoriaDado[:]
+        else:
+            print("O arquivo \"arquivoDiscoDados.txt\" nao existe!")
+        if(os.path.exists(arquivoDiscoLog)):
+            f = open(arquivoDiscoLog, "w")
+            for i in logMemoria:
+                f.write(str(i)+"\n")
+            f.write(str("<CHECKPOINT>\n"))
+            f.close
+        else:
+            print("O arquivo \"logDisco.txt\" nao existe!")   
 
-while aux != 's':
-    print("\na - Visualizar buffer do Log")
-    print("b - Visualizar log do disco")
-    print("c - Visualizar Dados da memoria")
-    print("d - Visualizar Dados do disco")
-    print("-------------------------------")
-    print("e - Update")
-    print("f - Checkpoint")
-    print("g - Falha")
-    print("h - Commit")
-    print("-------------------------------")
+def falha():
+    undo[:] += ([s for s in logMemoria if s not in redo])
+    print("previaMemoria:",previaMemoria)
+    for i in undo:
+        #print("ID:",i[:][1])
+        posicao = i[:][1]
+        #print("disco:",discoDado[posicao-1])
+        val = int(i[:][3])
+        discoDado[posicao-1] = val
+        previaMemoria[posicao-1] = val
+        #print("Teste apos falha:", discoDado)  
+
+    logMemoria.clear() 
+    memoriaDado.clear()   
+    print("previaMemoria apos falha:",previaMemoria,"\n\n\n")
+    with open('discodado.txt','w')as arquivoDiscoDados:
+            arquivoDiscoDados.write(str(previaMemoria))
+
+    
+
+def update():
+        numTransacao = input("Digite a transacao: ")
+        idValor = int(input("ID da pessoa: "))
+        velhoVal = discoDado[idValor - 1]
+        novoVal = int(input("Digite o valor: "))
+        logMemoria[:] += [(numTransacao,idValor,"salario",velhoVal,novoVal)]
+        memoriaDado[idValor - 1] = novoVal
+        print("Sucesso ao fazer Update!")
+#Transação | ID da pessoa | atributo | valor Antigo | Valor novo
+def menu():
+    print("MEMORIA:",memoriaDado)
+    print("Tx MEMORIA:",logMemoria,"\n")
+    print("a - Visualizar Log da memoria")
+    print("b - Visualizar Log do disco")
+    print("c - Update")
+    print("d - Checkpoint")
+    print("e - Falha")
+    print("f - Commit")
+    print("g - Visualizar dados no disco")
+    print("h - Visualizar dados na memoria")
+    print("i - Visualizar dados UNDO e REDO")
     print("s - Sair do programa")
-    print("-------------------------------")
-    aux = input("Escolha uma opcao: ")
+    print("Digite a opcao: ")
 
-    if aux == 'a':
-        viewLogMemoria()
-    elif aux == 'b':
-        viewLogDisco()
-    elif aux == 'c':
-        viewDadosMemoria()
-    elif aux == 'd':
-        viewDadosDisco()
-    elif aux == 'e':
+i = None
+while i != 's':
+    menu()
+    i = input().lower()
+    if i == 'a':
+        print(logMemoria)
+    elif i == 'b':
+        if(os.path.exists(arquivoDiscoLog)):
+            f = open(arquivoDiscoLog, "r")
+            print(f.read())
+            f.close
+        else:
+            print("O arquivo \"logDisco.txt\" nao existe!")
+    elif i == 'c':
         update()
-    elif aux == 'f':
+    elif i == 'd':
         checkpoint()
-    elif aux == 'g':
-        failure()
-    elif aux == 'h':
+    elif i == 'e':
+        #chamar Undo e Redo
+        falha()
+    elif i == 'f':
         commit()
+    elif i == 'g':
+        if(os.path.exists(arquivoDiscoDados)):
+            f = open(arquivoDiscoDados, "r")
+            print(f.read())
+            f.close
+            #print(discoDado)
+        else:
+            print("O arquivo \"arquivoDiscoDados.txt\" nao existe!")
+    elif i == 'h':
+        print(memoriaDado)
+    elif i == 'i':
+        print("REDO",redo,\
+              "\nUNDO",undo,
+              "mt-Var discoDado:",previaMemoria
+              )
     else:
-        print("Programa encerrado")
+        print("Saindo...")
         break
